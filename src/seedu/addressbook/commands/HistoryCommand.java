287453386ddb24ff5d60c42d0da6d6468287b91a
Ang YC <ang_yc@live.com>
2018-02-21 13:31:42 +0800

    Fix message usage inconsistency in HelpCommand

--- src/seedu/addressbook/commands/HistoryCommand.java
+++ src/seedu/addressbook/commands/HistoryCommand.java
@@ -6,7 +6,7 @@ package seedu.addressbook.commands;
 public class HistoryCommand extends Command {
 
     public static final String COMMAND_WORD = "history";
-    public static final String MESSAGE_USAGE = "Show command history in current session.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Show command history in current session.\n"
             + "Example: " + COMMAND_WORD;
 
     public HistoryCommand() {}
